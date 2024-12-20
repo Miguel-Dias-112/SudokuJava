471beb407c9c6c0299ef62d81fcaa8a850d68233
miguel <miguel.dias@estudante.ufjf.br>
2024-12-20 17:27:06 -0300

    Criação geração de um tabuleiro de sudoku

--- src/main/java/com/mycompany/sudoku/Tabuleiro.java
+++ src/main/java/com/mycompany/sudoku/Tabuleiro.java
@@ -4,15 +4,18 @@
  */
 package com.mycompany.sudoku;
 
+
 /**
  *
  * @author migue
  */
 public class Tabuleiro {
-    
     int tabuleiro[][]= new int[9][9];
+
+
     
-     public void Imprimir(){
+    
+    public void Imprimir(){
         System.out.println("======Tabuleiro======");
 
         for (int x = 0; x < 9; x++) {
@@ -29,7 +32,41 @@ public class Tabuleiro {
         }     
         System.out.println("=====================");
     }
-     public Tabuleiro(){
-         Imprimir();
-     }
+    private final void bauguncaNumeros() {
+        for (int i = 1; i <= 9; i++) { // Números de 1 a 9
+            int randomNumber = (int) (Math.random() * 10); // De 0 a 9
+            trocaNumeros(i, randomNumber); // Troca posições de i e ranNum
+        }
+    }
+    private void trocaNumeros(int n1, int n2) {
+        for (int y = 0; y<9; y++) {
+            for (int x = 0; x<9; x++) {
+                if (this.tabuleiro[x][y] == n1) {
+                    this.tabuleiro[x][y] = n2;
+                } else if (this.tabuleiro[x][y] == n2) {
+                    this.tabuleiro[x][y] = n1;
+                }
+            }   
+        }
+    }
+    
+    public Tabuleiro(){
+         int _tabuleiro[][]={
+                {1,2,3,  4,5,6,  7,8,9},
+                {4,5,6,  7,8,9,  1,2,3},
+                {7,8,9,  1,2,3,  4,5,6},
+
+                {2,3,1,  5,6,4,  8,9,7},
+                {5,6,4,  8,9,7,  2,3,1},
+                {8,9,7,  2,3,1,  5,6,4},
+
+                {3,1,2,  6,4,5,  9,7,8},
+                {6,4,5,  9,7,8,  3,1,2},
+                {9,7,8,  3,1,2,  6,4,5}
+         };
+         this.tabuleiro = _tabuleiro;
+         this.bauguncaNumeros();
+         this.Imprimir();
+         
+    }
 }
